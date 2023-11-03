70b536145e31d92247c0e65093c347ace31ef93c
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-11-03 15:50:01 +0100

    milestone 3

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,9 +1,13 @@
 package org.lessons.java;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("********* Enter a country or a part *********");
+        String searchUserCountry = scan.nextLine();
         String url = "jdbc:mysql://localhost:8889/db-nations";
         String user = "root";
         String password = "root";
@@ -11,10 +15,10 @@ public class Main {
         // creo la connessione
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             // scrivo la query e la assegno
-            String query = "SELECT c.country_id,c.name as Country_name, r.name as Region_name, c2.name as Continent_name FROM countries c JOIN regions r ON r.region_id = c.region_id  JOIN continents c2 ON c2.continent_id =r.continent_id ORDER BY c.name ASC;";
+            String query = "SELECT c.country_id,c.name as Country_name, r.name as Region_name, c2.name as Continent_name FROM countries c JOIN regions r ON r.region_id = c.region_id  JOIN continents c2 ON c2.continent_id =r.continent_id WHERE c.name LIKE ? ORDER BY c.name ASC;";
             //creo una statement sql e eseguo la query
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.execute();
+                preparedStatement.setString(1, "%" + searchUserCountry + "%");
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
                         String countryId = resultSet.getString("country_id");
@@ -25,9 +29,11 @@ public class Main {
                     }
                 } catch (SQLException e) {
                     System.out.println("unable to execute query");
+                    e.printStackTrace();
                 }
             } catch (SQLException e) {
                 System.out.println("unable to create Statement");
+                e.printStackTrace();
             }
         } catch (SQLException e) {
             System.out.println("unable to connect to database");
